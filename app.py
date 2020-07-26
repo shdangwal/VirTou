cd1d86ebabce54f35b4dca7f0a7322812234a1f2
Shubham Dangwal <shdangwal@gmail.com>
2020-07-26 17:44:48 +0530

    Adding Heroku required files for running.

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import request, jsonify
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-import json
 
 
 app = flask.Flask(__name__)
